25339d342fb9e66725e4fc8eca472a7f2e89679b
gCadurim <49075372+gCadurim@users.noreply.github.com>
2021-09-25 18:14:37 -0300

    Create cod.py

---
+++ cod.py
@@ -0,0 +1,10 @@
+frase1 = ('Rotator', 'bob', 'madam', 'mAlAyAlam', '1', 'Able was I, ere I saw Elba', 'Madam I’m Adam', 'Step on no pets', 'Top spot!', '02/02/2020')
+frase2 = ('xyz', 'elephant', 'Country', 'Top . post!', 'Wonderful-fool', 'Wild imagination!')
+
+def frase1():
+        return 'palíndromo'
+print('É um palíndromo')
+
+def frase2():
+        return 'nao palíndromo'
+print('Não é um palíndromo')
